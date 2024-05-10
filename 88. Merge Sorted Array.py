905c7265cd401aac0c12c40b9e5b233846cebcee
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-05-10 09:04:01 -0500

    Solution to LeetCode problem 88

---
+++ 88. Merge Sorted Array.py
@@ -0,0 +1,22 @@
+# URL: https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Creating a variable that has the length of the final list
+
+        # Sorting the two lists in nums1 itself
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m+n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m+n - 1] = nums2[n - 1]
+                n -= 1
+
+        # Dealing with the items leftout in nums2
+        while n > 0:
+            nums1[m+n - 1] = nums2[n - 1]
+            n -= 1
