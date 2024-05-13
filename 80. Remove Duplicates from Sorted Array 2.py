f288d0d7c144b07cf594c4bb13bcdcb9b4feaadd
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-05-13 16:01:20 -0500

    Solution for LeetCode problem 80

---
+++ 80. Remove Duplicates from Sorted Array 2.py
@@ -0,0 +1,17 @@
+#URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l,r = 0,0
+
+        while r in range(len(nums)):
+            count = 1
+            while (r+1)<len(nums) and nums[r] == nums[r+1]:
+                count += 1
+                r+=1
+            
+            for i in range(min(2, count)):
+                nums[l] = nums[r]
+                l+=1
+            r+=1
+        return l
